ec967f3c26f22630d905ec893090333b675da713
soekasir <soekasir@gmail.com>
2025-01-30 05:32:02 +0700

    fix: export react

---
+++ index.js
@@ -0,0 +1,6 @@
+/** Internal Dependencies */
+import FilerobotImageEditor,{ TABS, TOOLS } from './packages/react-filerobot-image-editor/src';
+
+export { TABS, TOOLS };
+
+export default FilerobotImageEditor;
